4b13858b240c06c9d8efaafe52b5638be876e3c1
NithyashreeK <39421236+NithyashreeK@users.noreply.github.com>
2018-10-06 00:27:29 -0400

    Update island.js

--- island.js
+++ island.js
@@ -1,25 +1,27 @@
 var arr = [[1,1,0,0,0], 
-				   [1,1,0,0,0],
-				   [0,0,1,0,0],
-				   [0,0,0,1,1]];
-		var count = 0;
-		for(var i=0; i<arr.length; i++){
-			for(var j=0; j<arr[i].length; j++){
-				if(arr[i][j]==1){
-					count++;
-					DFS(arr, i, j);
-				}				
-			}
-		}
-		console.log(count);
+	   [1,1,0,0,0],
+	   [0,0,1,0,0],
+	   [0,0,0,1,1]];
+
+var count = 0;
+
+for(var i=0; i<arr.length; i++){
+	for(var j=0; j<arr[i].length; j++){
+		if(arr[i][j]==1){
+			count++;
+			DFS(arr, i, j);
+		}				
+	}
+}
+console.log(count);
 		
     function DFS(arr, i, j){
-			if(i<0 || j<0 || i>=arr.length || j>= arr[i].length || (arr[i][j]!=1)){
-				return;
-			}
-			arr[i][j] = 0;
-			DFS(arr, i+1, j);
-			DFS(arr, i, j+1);
-			DFS(arr, i-1, j);
-			DFS(arr, i, j-1);
-		}
+	if(i<0 || j<0 || i>=arr.length || j>= arr[i].length || (arr[i][j]!=1)){
+		return;
+	}
+	arr[i][j] = 0;
+	DFS(arr, i+1, j);
+	DFS(arr, i, j+1);
+	DFS(arr, i-1, j);
+	DFS(arr, i, j-1);
+    }
