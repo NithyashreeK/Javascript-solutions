6fbacaf335b3ef22cb472a0cb52ba3d21c190c9b
NithyashreeK <39421236+NithyashreeK@users.noreply.github.com>
2018-10-06 20:54:32 -0400

    Update allStairs.js

--- allStairs.js
+++ allStairs.js
@@ -1,42 +1,42 @@
 //floyd inverted staircase
-		var prev = 1;
-		for(var i=5; i>0; i--){
-			var temp = '';
-			for(var j=0; j<i; j++){
-				temp = temp+' '+prev;
-				prev++;
-			}
-			console.log(temp);
-		}
+var prev = 1;
+for(var i=5; i>0; i--){
+	var temp = '';
+	for(var j=0; j<i; j++){
+		temp = temp+' '+prev;
+		prev++;
+	}
+	console.log(temp);
+}
 
 
-		//floyd staircase
-		var prev = 1;
-		for(var i=1; i<=5; i++){
-			var temp = ' ';
-			for(var j=0; j<i; j++){
-				temp = temp+' '+prev;
-				prev++;
-			}
-			console.log(temp);
-		}
+//floyd staircase
+var prev = 1;
+for(var i=1; i<=5; i++){
+	var temp = ' ';
+	for(var j=0; j<i; j++){
+		temp = temp+' '+prev;
+		prev++;
+	}
+	console.log(temp);
+}
 
-		// regular staircase
-		var prev = '1';
-		for(var i=1; i<=6; i++){
-			var temp = '';
-			for(var j=0; j<i; j++){
-				temp = prev+' '+temp;
-			}
-			console.log(temp);
-		}
+// regular staircase
+var prev = '1';
+for(var i=1; i<=6; i++){
+	var temp = '';
+	for(var j=0; j<i; j++){
+		temp = prev+' '+temp;
+	}
+	console.log(temp);
+}
 
-		//inverted staircase
-		var prev = '1';
-		for(var i=6; i>0; i--){
-			var temp = '';
-			for(var j=0; j<i; j++){
-				temp = prev+' '+temp;
-			}
-			console.log(temp);
-		}
+//inverted staircase
+var prev = '1';
+for(var i=6; i>0; i--){
+	var temp = '';
+	for(var j=0; j<i; j++){
+		temp = prev+' '+temp;
+	}
+	console.log(temp);
+}
