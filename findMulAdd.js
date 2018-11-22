76bcfac907fff822832a12e0914842db2dfe6774
NithyashreeK <39421236+NithyashreeK@users.noreply.github.com>
2018-11-22 00:43:51 -0500

    Update findMulAdd.js

--- findMulAdd.js
+++ findMulAdd.js
@@ -1,31 +1,31 @@
 function findMul(arr, target){ //find the unique numbers that multiply to get the target
-			var res = [];
-			var newarr = [];
-			for(var i=0; i<arr.length; i++){
-				if(target%arr[i] == 0){
-					var newnum = target/arr[i];
-					if((arr.includes(newnum))&&(!newarr.includes(arr[i]))) {
-						res.push([arr[i], newnum]);
-						newarr.push(newnum);
-					}
-					newarr.push(arr[i]);
-				}
+	var res = [];
+	var newarr = [];
+	for(var i=0; i<arr.length; i++){
+		if(target%arr[i] == 0){
+			var newnum = target/arr[i];
+			if((arr.includes(newnum))&&(!newarr.includes(arr[i]))) {
+				res.push([arr[i], newnum]);
+				newarr.push(newnum);
 			}
-			return res;
+			newarr.push(arr[i]);
+		}
+	}
+	return res;
 }
 
-	function toFindsum(arr, sum){ //all possible pairs of numbers summing up to the target
-			var pairs = [];
-			var numList = [];
+function toFindsum(arr, sum){ //all possible pairs of numbers summing up to the target
+	var pairs = [];
+	var numList = [];
 
-			for(var i = 0; i < arr.length; i++){
-				var curNum = arr[i];
-				var diff = sum - curNum;
-				if(numList.includes(diff)){
-					//console.log(numList, 'bnl');
-					pairs.push([curNum, diff]);
-				}
-				numList.push(curNum);
-			}
-			return pairs;
+	for(var i = 0; i < arr.length; i++){
+		var curNum = arr[i];
+		var diff = sum - curNum;
+		if(numList.includes(diff)){
+			//console.log(numList, 'bnl');
+			pairs.push([curNum, diff]);
+		}
+		numList.push(curNum);
 	}
+	return pairs;
+}
