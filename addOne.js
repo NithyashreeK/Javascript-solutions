a69effe0d211bdd57be651af8281c03df1737fca
NithyashreeK <39421236+NithyashreeK@users.noreply.github.com>
2018-12-10 23:31:14 -0500

    Update addOne.js

--- addOne.js
+++ addOne.js
@@ -1,19 +1,19 @@
 function addOne(arr){
-			var i = arr.length - 1;
-			while(i >= 0){
-				if(arr[i] != 9){
-					arr[i]++;
-					return arr;
-				}else{
-					arr[i] = 0;
-					i--;
-				}
+	var i = arr.length - 1;
+	while(i >= 0){
+		if(arr[i] != 9){
+			arr[i]++;
+			return arr;
+		}else{
+			arr[i] = 0;
+			i--;
+		}
 
-			}
-			if(arr[0] == 0){
-				arr.unshift(1);
-				return arr;
-			}else{
-				return arr;
-			}
+	}
+	if(arr[0] == 0){
+		arr.unshift(1);
+		return arr;
+	}else{
+		return arr;
+	}
 }
